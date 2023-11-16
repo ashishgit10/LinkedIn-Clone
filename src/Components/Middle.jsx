45277f55f5ec3a06ea379993e12c4b03239d55c8
ASHISH KUMAR SHARMA <ashishsharma.mail10@gmail.com>
2023-11-17 00:47:26 +0530

    Added Section

--- src/Components/Middle.jsx
+++ src/Components/Middle.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import proImg from "../assets/images/user.svg";
+import post from "../assets/images/post-photo.svg";
+import eventImg from "../assets/images/event.svg";
+import article from "../assets/images/article.svg";
+
 const Middle = () => {
   return (
     <Middleside>
-      <div>Share</div>
+      <div className="container">
+        <div className="topCont">
+          <div>
+            <img className="user" src={proImg}></img>
+          </div>
+          <div className="flex-grow">
+            <input type="text" placeholder="Start a post"></input>
+          </div>
+        </div>
+        <div className="imgList">
+          <a>
+            <img style={{ color: "#368fe8ff" }} src={post}></img>
+            <span>Media</span>
+          </a>
+          <a>
+            <img src={eventImg}></img>
+            <span>Event</span>
+          </a>
+          <a>
+            <img src={article}></img>
+            <span>Write Article</span>
+          </a>
+        </div>
+      </div>
     </Middleside>
   );
 };
@@ -15,5 +43,41 @@ const Middleside = styled.div`
   border-radius: 8px;
   padding: 12px;
   margin: 6px 0px;
+  .flex-grow {
+    display: flex;
+    flex: 1;
+  }
+  .topCont {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    input {
+      flex-grow: 1;
+      line-height: 40px;
+      border-radius: 20px;
+      padding-left: 10px;
+      margin-left: 15px;
+      max-width: 100%;
+      @media (max-width: 768px) {
+      }
+    }
+  }
+  .user {
+    width: 50px;
+    border-radius: 50%;
+  }
+  .imgList {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+    a {
+      display: flex;
+      align-items: center;
+      span {
+        padding-left: 8px;
+      }
+    }
+  }
 `;
 export default Middle;
